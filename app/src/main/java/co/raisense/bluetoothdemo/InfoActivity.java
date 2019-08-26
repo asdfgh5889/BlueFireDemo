bcbc37b8f83bc41ad8469590abf5d2ac34328c8b
Sherzod <asdfgh5889@gmail.com>
2019-08-26 18:19:12 +0500

    androidx migration

--- app/src/main/java/co/raisense/bluetoothdemo/InfoActivity.java
+++ app/src/main/java/co/raisense/bluetoothdemo/InfoActivity.java
@@ -1,11 +1,9 @@
 package co.raisense.bluetoothdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.TextView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
